b58c9283cc3371210ba4ee4dd9316e7fec85cedf
Gaurav Pandey <61927226+gauravgap@users.noreply.github.com>
2020-07-07 12:54:22 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,13 @@
+// Temp in kelvin
+kelvin = 293
+celsius = kelvin - 273;
+// Convert to celsius
+let fahrenheit = Math.floor(celsius * (9 / 5) + 32);
+// Displays Temp in farenheit 
+console.log(The temperature is ${fahrenheit} degrees Fahrenheit.);
+//Convert to Newton, rounded down
+let newton = Math.floor(celsius * (33/100));
+//Displays temp in Newton
+console.log(The temperature is ${newton} degrees Newton.);
+
+ 
