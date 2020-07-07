b9f8d492166422f32bc89248ec66a356ca5fd1ea
Gaurav Pandey <61927226+gauravgap@users.noreply.github.com>
2020-07-07 20:14:08 +0530

    Update project.js

--- project.js
+++ project.js
@@ -1,14 +1,14 @@
 //Temp in Kelvin
-const kelvin = 293;
+var kelvin = 293;
 //Convert to Celsius
-const celsius = kelvin - 273;
+var celsius = kelvin - 273;
 //Convert to Fahrenheit, rounded down
-let fahrenheit = celsius * (9/5) + 32;
+fahrenheit = celsius * (9/5) + 32;
 fahrenheit = Math.floor(fahrenheit);
 //Displays temp in Fahrenheit
 console.log(`The temperature is ${fahrenheit} degrees fahrenheit`);
 //Convert to Newton, rounded down
-let newton = celsius * (33/100);
+newton = celsius * (33/100);
 newton = Math.floor(newton);
 //Displays temp in Newton
 console.log(`The temperature is ${newton} degrees Newton`);
